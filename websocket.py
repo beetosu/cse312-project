20692ab39851171994d2d74ff2915f2909d6b2f0
beetosu <cpwalsh@buffalo.edu>
2021-11-04 12:23:51 -0400

    Add incredibly simple websocket server

---
+++ websocket.py
@@ -0,0 +1,14 @@
+import asyncio
+import websockets
+import json
+
+async def echo(websocket, path):
+    await websocket.send(json.dumps({"username": "you", "comment": "suck"}))
+    async for message in websocket:
+        await websocket.send(message)
+
+async def main():
+    async with websockets.serve(echo, "localhost", 8001):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
