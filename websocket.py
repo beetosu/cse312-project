495f3d653f238c884daf5c385f3ea4d26005b627
JODGEW <wenhaohe@buffalo.edu>
2021-11-07 13:39:00 -0500

    Write homepage and registration page HTML/CSS #3, #9

--- websocket.py
+++ websocket.py
@@ -2,10 +2,12 @@ import asyncio
 import websockets
 import json
 
+
 async def echo(websocket, path):
     await websocket.send(json.dumps({"username": "you", "comment": "suck"}))
     async for message in websocket:
         await websocket.send(message)
+
 
 async def main():
     async with websockets.serve(echo, "localhost", 8001):
