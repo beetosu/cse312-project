203e7fb39939849d955a0179f6f64014a6b313e1
beetosu <cpwalsh@buffalo.edu>
2021-11-20 13:57:32 -0500

    Add xsrf tokens

--- modify.py
+++ modify.py
@@ -1,3 +1,7 @@
+from secrets import token_hex
+
+xsrf_tokens = []
+
 def modify_request(reqObj):
     if reqObj['path'] == "/register":
         # do something
@@ -8,4 +12,9 @@ def modify_request(reqObj):
     return reqObj
 
 def modify_response(resObj):
+    if b'{{token}}' in resObj['body']:
+        token = token_hex(16)
+        xsrf_tokens.append(token)
+        resObj['body'] = resObj['body'].replace(b'{{token}}', bytes(token, 'utf-8'))
+        print(xsrf_tokens)
     return resObj
