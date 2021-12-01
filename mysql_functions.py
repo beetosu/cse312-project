4f85603a9b8d3733401bcfffe0823e00ef50f26d
Gabriel Treitmeier-McCarthy <gjtreitm@buffalo.edu>
2021-12-01 13:40:36 -0500

    Added function to check whether a user exists

--- mysql_functions.py
+++ mysql_functions.py
@@ -5,6 +5,7 @@ import mysql.connector
 dbuser = 'root'
 dbpw = 'changeme'
 dbname = 'classchat'
+dbhost = 'mysql'
 
 '''
 Creating userData table
@@ -49,9 +50,26 @@ WHERE LoggedIn = TRUE
  '''
 
 def db_init():
-    connection = mysql.connector.connect(user=dbuser, password=dbpw, database=dbname)
+    # Initializes server tables if they don't already exist.
+    connection = mysql.connector.connect(user=dbuser, password=dbpw, database=dbname, host=dbhost)
     cursor = connection.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS userData (id INT AUTO_INCREMENT PRIMARY KEY, username TEXT, password TEXT, FirstName TEXT, LastName TEXT, ProfilePictureUrl TEXT, LoggedIn BOOLEAN)")
     cursor.execute("CREATE TABLE IF NOT EXISTS messageData (id INT AUTO_INCREMENT PRIMARY KEY, channel TEXT, message TEXT, sender TEXT, recipient TEXT)")
     cursor.execute("CREATE TABLE IF NOT EXISTS userTokens (id INT AUTO_INCREMENT PRIMARY KEY, username TEXT, token TEXT)")
     connection.close()
+
+def db_check_user_exists(username: str) -> bool:
+    # Checks whether a username is already taken/the user exists.
+    connection = mysql.connector.connect(user=dbuser, password=dbpw, database=dbname, host=dbhost)
+    cursor = connection.cursor()
+    sqlToExecute = "SELECT * FROM userData WHERE username = %s"
+    usernameToCheck = (username, )
+    cursor.execute(sqlToExecute, usernameToCheck)
+    getData = cursor.fetchall()
+    # Return True if the user exists
+    if len(getData) >= 1:
+        connection.close()
+        return True
+    # Otherwise return False
+    connection.close()
+    return False
